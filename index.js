b024422e451759a2f156a6f0889d34ed08c27bda
roroshka <167912589+roroshka@users.noreply.github.com>
2024-04-23 22:15:22 +0300

    Update index.js

--- index.js
+++ index.js
@@ -143,7 +143,7 @@ client.once('ready', () => {
   setTimeout(() => {
     console.log('\x1b[32m%s\x1b[0m', `|    🎯 Activity sucessfully set!`);
     client.user.setPresence({
-      activities: [{ name: `Update Out V@1.5`, type: ActivityType.Watching }],
+      activities: [{ name: `с очком негра`, type: ActivityType.Plays }],
       status: 'idle',
     });
   }, 2000); 
