fc24df61c59be4df53bcf87a9874b9e2288a8ca5
roroshka <167912589+roroshka@users.noreply.github.com>
2024-04-23 22:46:41 +0300

    Update help.js

--- basicCommands/help.js
+++ basicCommands/help.js
@@ -1,6 +1,6 @@
-const { EmbedBuilder, ActionRowBuilder, ButtonBuilder } = require('discord.js');
-const { ButtonStyle } = require('discord.js');
+const { MessageEmbed, MessageActionRow, MessageButton } = require('discord.js');
 const db = require("../mongodb");
+
 module.exports = {
   name: 'help',
   aliases: ['hlp', 'h'],
@@ -9,48 +9,38 @@ module.exports = {
     const botUser = message.client.user;
     const botPing = Date.now() - message.createdTimestamp;
     const serverCount = message.client.guilds.cache.size;
-    const embed = new EmbedBuilder()
+
+    const embed = new MessageEmbed()
       .setColor('#2b71ec')
-      .setAuthor({
-        name: 'Я здесь, чтобы помочь!',
-        iconURL: 'https://cdn.discordapp.com/attachments/1175487983915376662/1175667506791325706/communication.png?ex=656c10b0&is=65599bb0&hm=e378f1b355a2401bcab504b08a0766001d6b7c090c91ce0a7a7a87c868feb955&', 
-        url: 'https://discord.gg/FUEHs7RCqz'
-    })
-      (
+      .setAuthor('Я здесь, чтобы помочь!', 'https://cdn.discordapp.com/attachments/1175487983915376662/1175667506791325706/communication.png?ex=656c10b0&is=65599bb0&hm=e378f1b355a2401bcab504b08a0766001d6b7c090c91ce0a7a7a87c868feb955&', 'https://discord.gg/FUEHs7RCqz')
+      .setDescription('Here are some available commands:')
+      .addFields(
         // Basic commands category
         // Music commands category
-        {
-          name: '▶️  Music',
-          value: '`play`, `stop`, `history`,`volume`,`pause`,`resume`,`247`',
-          inline: true,
-        },
-        //fun category
-        //image category
-        //anime category
-        // Utility commands category
-      
+        { name: '▶️  Music', value: '`play`, `stop`, `history`, `volume`, `pause`, `resume`, `247`', inline: true }
+        // Add more fields for other categories as needed
       )
       .setThumbnail(botUser.avatarURL({ dynamic: true, format: 'png', size: 1024 }))
-      .setImage(`https://cdn.discordapp.com/attachments/1170652474563117138/1224660177039396876/MULTI_MUSIC_v1.5.png?ex=661e4cab&is=660bd7ab&hm=f7a542462fabaf9c1530ece5aa72597cff3ac032876bba46df5ddba7e122ea99&`);
+      .setImage('https://cdn.discordapp.com/attachments/1170652474563117138/1224660177039396876/MULTI_MUSIC_v1.5.png?ex=661e4cab&is=660bd7ab&hm=f7a542462fabaf9c1530ece5aa72597cff3ac032876bba46df5ddba7e122ea99&');
 
-    const button1 = new ButtonBuilder()
+    const button1 = new MessageButton()
       .setLabel('YouTube')
       .setURL('https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A')
-      .setStyle(ButtonStyle.Link);
+      .setStyle('LINK');
 
-    const button2 = new ButtonBuilder()
+    const button2 = new MessageButton()
       .setLabel('Discord')
       .setURL('https://discord.gg/FUEHs7RCqz')
-      .setStyle(ButtonStyle.Link);
+      .setStyle('LINK');
 
-    const button3 = new ButtonBuilder()
+    const button3 = new MessageButton()
       .setLabel('Code')
       .setURL('https://replit.com/@BEASTGAMERS1/MULTI-MUSIC-BOT-v200?v=1')
-      .setStyle(ButtonStyle.Link);
-      
-    const row = new ActionRowBuilder()
+      .setStyle('LINK');
+
+    const row = new MessageActionRow()
       .addComponents(button1, button2, button3);
-    
+
     message.reply({ embeds: [embed], components: [row] });
   },
 };
